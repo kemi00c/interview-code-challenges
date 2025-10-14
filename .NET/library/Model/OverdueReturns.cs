9f286ea90c057d830d0aa71f9c2ebb85e34f3604
Adam Kemenes <kemi242@gmail.com>
2025-10-14 18:10:20 +0200

    Added tracking for overdue returns

---
+++ .NET/library/Model/OverdueReturns.cs
@@ -0,0 +1,9 @@
+﻿namespace OneBeyondApi.Model
+{
+    public class OverdueReturns
+    {
+        public Guid Id { get; set; }
+        public Borrower? Borrower { get; set; }
+        public int NumberOfOverdueReturns { get; set; }
+    }
+}
